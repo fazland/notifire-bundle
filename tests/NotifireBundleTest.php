bb72980646f59edee264a7849d2e17b99bbe7e11
Stefano Rainieri <stefano.rainieri@fazland.com>
2016-05-26 17:58:16 +0200

    Test for SwiftMailer auto_config.

---
+++ tests/NotifireBundleTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Fazland\NotifireBundle\Tests;
+
+use Fazland\NotifireBundle\Tests\Fixtures\AppKernel;
+use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @author Massimiliano Braglia <massimiliano.braglia@fazland.com>
+ */
+class NotifireBundleTest extends WebTestCase
+{
+    protected static function createKernel(array $options = array())
+    {
+        return new AppKernel('test', true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        $fs = new Filesystem();
+        $fs->remove(__DIR__ . '/../Fixtures/Translations/cache');
+        $fs->remove(__DIR__ . '/../Fixtures/Translations/logs');
+    }
+
+    public function testThisIsATest()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $response = $client->getResponse();
+        $this->assertTrue(true);
+    }
+    
+    public function testSwiftMailerAutoConfiguration()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+        
+        $this->assertNotEmpty($container->get('fazland.notifire.handler.swiftmailer.first_mailer'));
+    }
+}
