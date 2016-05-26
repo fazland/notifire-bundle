48ddf547ea69bff458ee64bbcaa1f448224191c7
Stefano Rainieri <stefano.rainieri@fazland.com>
2016-05-26 18:36:14 +0200

    SwiftMailerHandlerConfig test TwilioServiceHandlerConfig test.

--- tests/NotifireBundleTest.php
+++ tests/NotifireBundleTest.php
@@ -2,6 +2,8 @@
 
 namespace Fazland\NotifireBundle\Tests;
 
+use Fazland\Notifire\EventSubscriber\Email\SwiftMailerHandler;
+use Fazland\Notifire\EventSubscriber\Sms\TwilioHandler;
 use Fazland\NotifireBundle\Tests\Fixtures\AppKernel;
 use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
 use Symfony\Component\Filesystem\Filesystem;
@@ -35,11 +37,26 @@ class NotifireBundleTest extends WebTestCase
         $this->assertTrue(true);
     }
     
-    public function testSwiftMailerAutoConfiguration()
+    public function testSwiftMailerHandlerConfiguration()
     {
         $client = static::createClient();
         $container = $client->getContainer();
         
         $this->assertNotEmpty($container->get('fazland.notifire.handler.swiftmailer.first_mailer'));
+        $this->assertInstanceOf(SwiftMailerHandler::class, $container->get('fazland.notifire.handler.swiftmailer.first_mailer'));
+
+        $this->assertNotEmpty($container->get('fazland.notifire.handler.swiftmailer.second_mailer'));
+        $this->assertInstanceOf(SwiftMailerHandler::class, $container->get('fazland.notifire.handler.swiftmailer.second_mailer'));
+    }
+
+    public function testTwilioServiceHandlerConfiguration()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+
+        $name = $container->getParameter('twilio_name');
+
+        $this->assertNotEmpty($container->get("fazland.notifire.handler.twilio.$name"));
+        $this->assertInstanceOf(TwilioHandler::class, $container->get("fazland.notifire.handler.twilio.$name"));
     }
 }
